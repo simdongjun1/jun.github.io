e79316d235674f227de49f59ce344f28bcd91d0e
simdongjun1 <bbs1001kr@naver.com>
2024-07-08 03:16:18 +0900

    바rld

--- script.js
+++ script.js
@@ -20,3 +20,13 @@ function moveSlide(n) {
 }
 
 document.addEventListener('DOMContentLoaded', showSlides);
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
